7f43276b35e41038433db204b9c38e2449f1e299
JanKaram2020 <65977048+JanKaram2020@users.noreply.github.com>
2021-03-16 22:46:24 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: "final-stack",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "t6xuw7ui",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-22GD9Y13NJ",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
