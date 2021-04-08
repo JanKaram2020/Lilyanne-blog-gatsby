94e3ec11fad753f02c4c5d2b26133da1d2737a4c
JanKaram2020 <65977048+JanKaram2020@users.noreply.github.com>
2021-04-08 15:57:38 +0200

    made the logo only svg

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,108 +1,101 @@
-import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
-import { Grid, useColorMode } from 'theme-ui';
+import { useColorMode } from 'theme-ui';
 import Theme from '../gatsby-plugin-theme-ui/index';
 
 function Logo() {
-  const SVGSupported = typeof SVGRect !== 'undefined';
   const [colorMode] = useColorMode();
   const dark = colorMode === 'dark';
   const color = dark ? Theme.colors.modes.dark.primary : Theme.colors.primary;
-  if (SVGSupported) {
-    return (
-      <>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 877.65 437.95">
-          <path
-            fill="none"
-            stroke={color}
-            strokeMiterlimit="10"
-            strokeWidth="17"
-            d="M144.5 -181.2L144.5 -181.2 144.5 -181.2 144.5 -181.2 144.5 -181.2z"
-          />
-          <rect
-            width="11.46"
-            height="311.48"
-            y="50.82"
-            rx="5.73"
-            ry="5.73"
-            fill={color}
-          />
-          <rect
-            width="11.46"
-            height="311.48"
-            x="237.01"
-            y="467.53"
-            rx="5.73"
-            ry="5.73"
-            transform="rotate(-90 65.89 533.42)"
-            fill={color}
-          />
-          <rect
-            width="11.46"
-            height="436.75"
-            x="282.63"
-            y="219.05"
-            rx="5.73"
-            ry="5.73"
-            transform="rotate(-45 -77.077 409.096)"
-            fill={color}
-          />
-          <rect
-            width="11.46"
-            height="311.48"
-            x="432.87"
-            y="281.79"
-            rx="5.73"
-            ry="5.73"
-            transform="rotate(180 395.095 304.175)"
-            fill={color}
-          />
-          <rect
-            width="11.46"
-            height="311.48"
-            x="282.86"
-            y="131.78"
-            rx="5.73"
-            ry="5.73"
-            transform="rotate(90 378.435 110.665)"
-            fill={color}
-          />
-          <rect
-            width="11.46"
-            height="168.55"
-            x="348.25"
-            y="336.06"
-            rx="5.73"
-            ry="5.73"
-            transform="rotate(90 443.835 243.485)"
-            fill={color}
-          />
-          <circle
-            cx="183.72"
-            cy="188.69"
-            r="121.02"
-            fill="none"
-            stroke={color}
-            strokeMiterlimit="10"
-            strokeWidth="17"
-          />
-          <text
-            fontFamily="Pristina-Regular, Pristina"
-            fontSize="150"
-            transform="translate(377.84 148.11)"
-            fill={color}
-          >
-            Lilyanne
-            <tspan x="0" y="211.2">
-              Hany
-            </tspan>
-          </text>
-        </svg>
-      </>
-    );
-  }
   return (
-    <StaticImage src="../images/lola's logo.png" alt="Lilyanne Hany's logo" />
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 877.65 437.95">
+        <path
+          fill="none"
+          stroke={color}
+          strokeMiterlimit="10"
+          strokeWidth="17"
+          d="M144.5 -181.2L144.5 -181.2 144.5 -181.2 144.5 -181.2 144.5 -181.2z"
+        />
+        <rect
+          width="11.46"
+          height="311.48"
+          y="50.82"
+          rx="5.73"
+          ry="5.73"
+          fill={color}
+        />
+        <rect
+          width="11.46"
+          height="311.48"
+          x="237.01"
+          y="467.53"
+          rx="5.73"
+          ry="5.73"
+          transform="rotate(-90 65.89 533.42)"
+          fill={color}
+        />
+        <rect
+          width="11.46"
+          height="436.75"
+          x="282.63"
+          y="219.05"
+          rx="5.73"
+          ry="5.73"
+          transform="rotate(-45 -77.077 409.096)"
+          fill={color}
+        />
+        <rect
+          width="11.46"
+          height="311.48"
+          x="432.87"
+          y="281.79"
+          rx="5.73"
+          ry="5.73"
+          transform="rotate(180 395.095 304.175)"
+          fill={color}
+        />
+        <rect
+          width="11.46"
+          height="311.48"
+          x="282.86"
+          y="131.78"
+          rx="5.73"
+          ry="5.73"
+          transform="rotate(90 378.435 110.665)"
+          fill={color}
+        />
+        <rect
+          width="11.46"
+          height="168.55"
+          x="348.25"
+          y="336.06"
+          rx="5.73"
+          ry="5.73"
+          transform="rotate(90 443.835 243.485)"
+          fill={color}
+        />
+        <circle
+          cx="183.72"
+          cy="188.69"
+          r="121.02"
+          fill="none"
+          stroke={color}
+          strokeMiterlimit="10"
+          strokeWidth="17"
+        />
+        <text
+          fontFamily="Pristina-Regular, Pristina"
+          fontSize="150"
+          transform="translate(377.84 148.11)"
+          fill={color}
+        >
+          Lilyanne
+          <tspan x="0" y="211.2">
+            Hany
+          </tspan>
+        </text>
+      </svg>
+    </>
   );
 }
 
